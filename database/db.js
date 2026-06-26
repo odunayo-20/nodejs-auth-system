74a87366eb95ab47766792e324808c18183179c0
mecbilltech <mecbilltech@gmail.com>
2026-06-26 09:17:20 +0100

    little changes in the db

--- database/db.js
+++ database/db.js
@@ -5,7 +5,9 @@ const connectToDB = async () => {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection fialed");
+    console.error("MongoDB connection fialed").catch((err) => {
+      console.error("MongoDB connection failed:", err);
+    });
     process.exit(1);
   }
 };
